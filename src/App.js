d865a803f596325de6a65bed5ed2f5c5d2ad14e6
foreveralone1608 <hotranphihung@gmail.com>
2022-10-20 19:24:30 +0700

    add components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import React from "react";
+import TodoList from "./TodoList";
 
 function App() {
-  return null
+  return (<TodoList />)
 }
 
 export default App;
---
+++ src/TodoList.js
@@ -0,0 +1,13 @@
+//type rfc
+import React from 'react'
+
+export default function TodoList() {
+  return (
+    <>
+        <div>TodoList</div>
+        <h1>Hello</h1>
+        <button>Button</button>
+    </>
+    
+  )
+}
