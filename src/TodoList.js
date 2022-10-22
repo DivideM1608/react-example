252214abd1cdfab9d56e1c54e2f99e6ecb26e3d6
foreveralone1608 <hotranphihung@gmail.com>
2022-10-22 17:15:16 +0700

    add useState useEffect useRef

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,12 +1,48 @@
 //type rfc
-import React from 'react'
+import React, {useEffect, useState, useRef} from 'react'
 
 export default function TodoList() {
+
+  //ref for single
+  const[countOne, setCountOne] = useState(4)
+  function increateCountOne() {
+    setCountOne(prevCount => {
+      return {...prevCount, count: prevCount.count + 1}
+    })
+  }
+  
+  //ref for multiple 
+  const [state, setState] = useState({count: 4, theme: "blue"})
+  const count = state.count
+  const theme = state.theme
+
+  function increasementCount() {
+    setState(prevState => {
+      return {...prevState, count: prevState.count + 1}
+    })
+  }
+  function degrementCount() {
+    setState(prevState => {
+      return {...prevState, count: prevState.count - 1}
+    })
+
+
+  } 
+  //
+  const prevNumber = useRef()
+  useEffect(() => {
+    prevNumber.current = state.count
+  })
+
+
   return (
     <>
         <div>TodoList</div>
-        <h1>Hello</h1>
-        <button>Button</button>
+        <h1>{count}</h1>
+        <button onClick={increasementCount}>+</button>
+        <button onClick={degrementCount}>-</button>
+        <h2>{theme}</h2>
+        <h2>{prevNumber.current}</h2>
     </>
     
   )
